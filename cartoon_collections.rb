a0706354e31e24d9d2ebdda637c635e5468dd0ab
jacinta246 <jacinta246@yahoo.com>
2020-06-17 18:50:32 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,15 @@
+require 'pry'
 def greet_characters(array)
-  # Use `each` to enumerate over the provided array
-  #
-  # Print a custom greeting for each element
+  array.each do |array|
+    puts "Hello #{array}!"
+end
 end
 
 def list_dwarves(array)
+  array.each_with_index { |name, index| 
+  puts " #{index+1} #{name} "
+}
+#binding.pry}
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
